25895a538ee6c911b48b45896c3cf9cf7074774c
vishalgoyal3004 <vishal.goyallive@gmail.com>
2024-09-26 09:48:32 +0100

    To remove uused code.

--- src/pages/Reservation.jsx
+++
@@ -1,7 +0,0 @@
-export class Reservation {
-  constructor(car, startDatetime, endDateTime) {
-    this.car = car;
-    this.startDatetime = new Date(startDatetime);
-    this.endDateTime = new Date(endDateTime);
-  }
-}
